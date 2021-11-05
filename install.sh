3774606788fb2388f1af0f8fb7166be67f134816
Mattias van de Hoef <mattias.vandehoef@prolucid.ca>
2021-11-05 15:20:30 -0400

    first script

---
+++ install.sh
@@ -0,0 +1,18 @@
+C:\FactoryServer
+
+curl -sSL https://repos.insights.digitalocean.com/install.sh | sudo bash
+
+useradd -m steam
+cd /home/steam
+
+sudo add-apt-repository multiverse
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install lib32gcc1 steamcmd 
+
+sudo apt install steamcmd
+
+ln -s /usr/games/steamcmd steamcmd
+
+steamcmd +login anonymous +force_install_dir FactoryServer SatisfactoryDedicatedServer +app_update 1690800 +quit
+FactoryServer.exe -log -unattended
